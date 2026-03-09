5cbfd6320a891212efc8cb79f9e3bf4a5d1baf1b
Adham Gad <vfvendetta123@gmail.com>
2026-03-09 03:10:43 -0700

    Add leaky integrate-and-fire neuron

---
+++ src/lif.v
@@ -0,0 +1,32 @@
+`default_nettype 
+
+module lif(
+    input wire [7:0] current,
+    input wire       clk,
+    input wire       reset_n,
+    output reg [7:0] state,
+    output wire      spike,
+)
+
+wire [7:0] next_state;
+reg  [7:0] threshhold;
+;
+
+always @(posedge clk) begin
+    
+    if (!reset_n) begin
+        state <= 0;
+        threshhold <= 200;
+        
+    end else begin
+        state <= next_state;
+end
+
+end
+// next state logic
+assign next_state = current + (state >> 1);
+
+//spiking logic
+assign spike = (state >= threshhold);
+
+endmodule
